3f7abc8353adbc3f236c73f60c573989f403602b
Belli <ivanlorenzana@outlook.com>
2020-07-10 19:16:33 -0700

    Actualizacion 6

--- Cliente.java
+++ Cliente.java
@@ -6,123 +6,149 @@ import java.awt.*;
 import java.awt.event.*;
 import java.net.*;
 import java.io.*;
+import java.awt.Color;
+import java.awt.Font;
 
 public class Cliente extends JFrame implements ActionListener, Runnable{
-	public JTextField camposTxt[] = new JTextField[9];
 	public JPanel panel;
-	public JButton btnEnviar;
-	public JLabel lblTexto;
-	public JLabel lblNombre;
-	public JLabel lblNss;
-	public JLabel lblSangre;
-	public JLabel lblGenero;
-	public JLabel lblNacionalidad;
-	public JLabel lblEstCivil;
-	public JLabel lblCel;
-	public JLabel lblEnfermedad;
-	public JLabel lblCurp;
-	public JLabel lblV;
-	public JLabel lblN;
-	public JLabel lblO;
-	public JLabel lblP;
-	public JLabel lblFondo;// lbl para el fondo que pondras amiko
-	public Color cAzul= new Color(208,231,227);
-	public Color cGris= new Color(187,211,216);
+ 	public JLabel lblNombre;
+ 	public JLabel lblCel;
+ 	public JLabel lblMail;
+ 	public JLabel lblSangre;
+ 	public JLabel lblGenero;
+ 	public JLabel lblFecha;
+ 	public JLabel lblEnfermedad;
+ 	public JLabel lblCurp;
+ 	public JLabel lblDom;
+ 	public JLabel lblTitulo;
+ 	public JLabel fondo;
+ 	public JButton btnRegresar;
+ 	public JButton btnRegistro;
+	public JTextField camposTxt[] = new JTextField[9];
 	public Boolean respuestaServidor = new Boolean(false);
 	public Thread hilo1;
+	public Color color1= new Color(7,3,26);//Azul oscuro
+	public Color color2= new Color(255,203,116);//AMARILLO
+	public Color color3= new Color(79,138,139);// Azul claro
+	public Color color4= new Color(244,246,255);// blanco azulado
 
 	public Cliente(){
-		panel = new JPanel();
+		panel= new JPanel();
 		panel.setLayout(null);
-		panel.setBackground(cGris);
+		panel.setBackground(color1);
 
- 		lblN=new JLabel(new ImageIcon(((new ImageIcon("./imagenes/st.png")).getImage()).getScaledInstance(70,70,java.awt.Image.SCALE_SMOOTH)));
- 		lblN.setBounds(450,30,70,120);
+		fondo=new JLabel("");
+		fondo.setBounds(0,320,600,10);
+		fondo.setBackground(color2);
+		fondo.setOpaque(true);
 
- 		lblO=new JLabel(new ImageIcon(((new ImageIcon("./imagenes/med.png")).getImage()).getScaledInstance(70,70,java.awt.Image.SCALE_SMOOTH)));
- 		lblO.setBounds(450,150,70,120);
+		lblTitulo= new JLabel("Ingrese los siguientes datos",SwingConstants.CENTER);
+		lblTitulo.setBounds(0,0,600,30);
+		lblTitulo.setBackground(color3);
+		lblTitulo.setOpaque(true);
+		lblTitulo.setForeground(Color.white);
 
- 		lblP=new JLabel(new ImageIcon(((new ImageIcon("./imagenes/doc.png")).getImage()).getScaledInstance(70,70,java.awt.Image.SCALE_SMOOTH)));
- 		lblP.setBounds(450,270,70,120);
+		camposTxt[0]= new JTextField(50);
+		camposTxt[0].setBounds(100,40,240,30);
+		camposTxt[0].setBackground(color3);
+		camposTxt[0].setForeground(color1);
+		lblNombre= new JLabel(" Nombre: ", SwingConstants.CENTER);
+		lblNombre.setBounds(20,40,80,30);
+		lblNombre.setBackground(color4);
+		lblNombre.setOpaque(true);
 
-		lblTexto=new JLabel(" Ingrese los siguientes datos: ");
-		lblTexto.setBounds(195,0,200,30);
-
-		lblNombre=new JLabel("Nombre: ");
-		lblNombre.setBounds(5,40,60,30);
-		camposTxt[0]=new JTextField(50);
-		camposTxt[0].setBounds(150,40,225,30);
-		camposTxt[0].setBorder(null);
-
-		lblNss=new JLabel("Celular: ");
-		lblNss.setBounds(5,80,60,30);
 		camposTxt[1]=new JTextField(10);
-		camposTxt[1].setBounds(150,80,225,30);
-		camposTxt[1].setBorder(null);
+		camposTxt[1].setBounds(420,40,150,30);
+		camposTxt[1].setBackground(color3);
+		camposTxt[1].setForeground(color1);
+		lblCel= new JLabel(" Celular: ",SwingConstants.CENTER);
+		lblCel.setBounds(350,40,70,30);
+		lblCel.setBackground(color4);
+		lblCel.setOpaque(true);
 
-		lblSangre=new JLabel("E-mail: ");
-		lblSangre.setBounds(5,120,100,30);
-		camposTxt[2]=new JTextField(50);
-		camposTxt[2].setBounds(150,120,225,30);
-		camposTxt[2].setBorder(null);
+		camposTxt[2]=new JTextField(100);
+		camposTxt[2].setBounds(100,80,470,30);
+		camposTxt[2].setBackground(color3);
+		camposTxt[2].setForeground(color1);
+		lblDom= new JLabel(" Domicilio: ",SwingConstants.CENTER);
+		lblDom.setBounds(20,80,80,30);
+		lblDom.setBackground(color4);
+		lblDom.setOpaque(true);
 
+		camposTxt[3]= new JTextField(15);
+		camposTxt[3].setBounds(70,120,150,30);
+		camposTxt[3].setBackground(color3);
+		camposTxt[3].setForeground(color1);
+		lblCurp= new JLabel(" Curp: ",SwingConstants.CENTER);
+		lblCurp.setBounds(20,120,50,30);
+		lblCurp.setBackground(color4);
+		lblCurp.setOpaque(true);
 
-		lblGenero=new JLabel("Tipo Sangre: ");
-		lblGenero.setBounds(5,160,100,30);
-		camposTxt[3]=new JTextField(3);
-		camposTxt[3].setBounds(150,160,225,30);
-		camposTxt[3].setBorder(null);
+		camposTxt[4]= new JTextField(50);
+		camposTxt[4].setBounds(300,120,270,30);
+		camposTxt[4].setBackground(color3);
+		camposTxt[4].setForeground(color1);
+		lblMail= new JLabel(" E-mail: ",SwingConstants.CENTER);
+		lblMail.setBounds(230,120,70,30);
+		lblMail.setBackground(color4);
+		lblMail.setOpaque(true);
 
-		lblNacionalidad= new JLabel("Genero:");
-		lblNacionalidad.setBounds(5,200,100,30);
-		camposTxt[4]=new JTextField(10);
-		camposTxt[4].setBounds(150,200,225,30);
-		camposTxt[4].setBorder(null);
+		camposTxt[5]= new JTextField(5);
+		camposTxt[5].setBounds(120,160,40,30);
+		camposTxt[5].setBackground(color3);
+		camposTxt[5].setForeground(color1);
+		lblSangre= new JLabel(" Tipo de sangre: ",SwingConstants.CENTER);
+		lblSangre.setBounds(20,160,100,30);
+		lblSangre.setBackground(color4);
+		lblSangre.setOpaque(true);
 
-		lblEstCivil=new JLabel("Fecha Nacimiento: ");
-		lblEstCivil.setBounds(5,240,100,30);
-		camposTxt[5]=new JTextField(10);
-		camposTxt[5].setBounds(150,240,225,30);
-		camposTxt[5].setBorder(null);
+		camposTxt[6]= new JTextField(10);
+		camposTxt[6].setBounds(230,160,120,30);
+		camposTxt[6].setBackground(color3);
+		camposTxt[6].setForeground(color1);
+		lblGenero= new JLabel(" Genero: ",SwingConstants.CENTER);
+		lblGenero.setBounds(170,160,60,30);
+		lblGenero.setBackground(color4);
+		lblGenero.setOpaque(true);
 
-		lblCel= new JLabel("Domicilio: ");
-		lblCel.setBounds(5,280,100,30);
-		camposTxt[6]=new JTextField(50);
-		camposTxt[6].setBounds(150,280,225,30);
-		camposTxt[6].setBorder(null);
+		camposTxt[7]= new JTextField(11);
+		camposTxt[7].setBounds(450,160,120,30);
+		camposTxt[7].setBackground(color3);
+		camposTxt[7].setForeground(color1);
+		lblFecha= new JLabel("Nacimiento:",SwingConstants.CENTER);
+		lblFecha.setBounds(360,160,90,30);
+		lblFecha.setBackground(color4);
+		lblFecha.setOpaque(true);
 
-		lblEnfermedad=new JLabel("Alergias: ");
-		lblEnfermedad.setBounds(5,320,190,30);
-		camposTxt[7]=new JTextField(20);
-		camposTxt[7].setBounds(150,320,225,30);
-		camposTxt[7].setBorder(null);
+		camposTxt[8]= new JTextField(100);
+		camposTxt[8].setBounds(130,200,440,30);
+		camposTxt[8].setBackground(color3);
+		camposTxt[8].setForeground(color1);
+		lblEnfermedad= new JLabel(" Enfermedades: ",SwingConstants.CENTER);
+		lblEnfermedad.setBounds(20,200,110,30);
+		lblEnfermedad.setBackground(color4);
+		lblEnfermedad.setOpaque(true);
 
-		lblCurp=new JLabel ("Enfermedades: ");
-		lblCurp.setBounds(5,360,100,30);
-		camposTxt[8]=new JTextField(50);
-		camposTxt[8].setBounds(150,360,225,30);
-		camposTxt[8].setBorder(null);
+		btnRegistro= new JButton("Enviar");
+		btnRegistro.setBounds(120,250,175,50);
+		btnRegistro.setBackground(color4);
+		btnRegistro.addActionListener(this);
+		panel.add(lblTitulo);
 
-		btnEnviar=new JButton("Enviar");
-		btnEnviar.setBounds(255,410,250,30);
-		btnEnviar.addActionListener(this);
-		btnEnviar.setBackground(cAzul);
-
-		panel.add(btnEnviar);
 		panel.add(camposTxt[0]);panel.add(camposTxt[1]);panel.add(camposTxt[2]);panel.add(camposTxt[3]);panel.add(camposTxt[4]);panel.add(camposTxt[5]);panel.add(camposTxt[6]);panel.add(camposTxt[7]);panel.add(camposTxt[8]);
-		panel.add(lblTexto);panel.add(lblNombre);panel.add(lblNss);panel.add(lblSangre);panel.add(lblGenero);panel.add(lblNacionalidad);panel.add(lblEstCivil);panel.add(lblCel);panel.add(lblEnfermedad);panel.add(lblCurp);
+		panel.add(lblNombre);panel.add(lblCel);panel.add(lblDom);panel.add(lblCurp);panel.add(lblMail);panel.add(lblSangre);panel.add(lblGenero);panel.add(lblEnfermedad);panel.add(lblFecha);
+		panel.add(btnRegistro);
+		panel.add(fondo);
 
-		panel.add(lblN);panel.add(lblO);panel.add(lblP);
 		hilo1 = new Thread(this);
-
 		this.add(panel);
-		this.setTitle("Cliente");
-		this.setBounds(500,200,600,500);
+		this.setTitle("Registro");
+		this.setBounds(100,150,600,370);
 		this.setVisible(true);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 	public void actionPerformed(ActionEvent event){
-		if(event.getSource() == this.btnEnviar){
+		if(event.getSource() == this.btnRegistro){
 			int cont = 0;
 			for(int i=0; i<camposTxt.length; i++) {
 				if(camposTxt[i].getText().isEmpty()){
@@ -130,7 +156,7 @@ public class Cliente extends JFrame implements ActionListener, Runnable{
 				}
 			}
 			if(cont==0){
-				btnEnviar.setEnabled(false);
+				btnRegistro.setEnabled(false);
 				hilo1.start();
 			}else{
 				JOptionPane.showMessageDialog(null,"Favor de llenar todos los campos");
